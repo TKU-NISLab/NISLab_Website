c6afc459d910dd47c2603d7238d44a3f444c7074
mrmoneyc <mmc@mrmoneyc.org>
2015-02-13 18:49:47 +0800

    MODIFY: home_slider photo ADD: Sitemap

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,9 @@ $(document).ready( function () {
 	  "/img/home_slider/0004.jpg",
 	  "/img/home_slider/0005.jpg",
 	  "/img/home_slider/0006.jpg",
-	  "/img/home_slider/0007.jpg"
+	  "/img/home_slider/0007.jpg",
+	  "/img/home_slider/0008.jpg",
+	  "/img/home_slider/0009.jpg",
+	  "/img/home_slider/0010.jpg"
 	], {duration: 3000, fade: 750});
 });
